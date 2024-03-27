1a7f062c42488959e48a5dbc8e9322f7e1e4977a
Antonio Pantoja <tato_fos@outlook.com>
2024-03-27 18:01:04 -0400

    14. Add additional routes

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <>
+      <span className="text-7xl">About Page</span>
+    </>
+  )
+
+}
---
+++ app/pricing/page.tsx
@@ -0,0 +1,7 @@
+export default function PricingPage() {
+  return (
+    <>
+      <span className="text-6xl">Pricing Page</span>
+    </>
+  )
+}
