23ebae1652fa7760fed804fc54ae679c75ee6e81
Antonio Pantoja <tato_fos@outlook.com>
2024-03-28 19:46:03 -0400

    17. Add navbar component
    - Added Navbar component to the general layout.

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,10 +1,15 @@
+import { Navbar } from "@/components"
+
 export default function GeneralLayout({ children }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <main className="flex flex-col items-center p-24">
-      <span className="text-lg">Hello Root Layout General</span>
-      { children }
-    </main>
+    <>
+      <Navbar />
+      <main className="flex flex-col items-center p-24">
+        <span className="text-lg">Hello Root Layout General</span>
+        { children }
+      </main>
+    </>
   );
 }
