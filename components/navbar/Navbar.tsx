9da285665fe89ed405b62bf15b07da7534222f75
Antonio Pantoja <tato_fos@outlook.com>
2024-03-29 22:20:32 -0400

    19. Add Next Link
    - Add octoicons

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,17 +1,37 @@
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+
+const navItems = [
+  {path:'/about', text:'About'},
+  {path:'/pricing', text:'Pricing'},
+  {path:'/contact', text:'Contact'}
+];
 
 export const Navbar = () => {
   console.log('%cNavbar is a Server Component working on the server side', 'color:cyan');
 
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link className="flex items-center ml-2" href={'/'}>
+        <HomeIcon className="mr-2" />
+        <span>Home</span>
+      </Link>
 
       <div className="flex flex-1"></div>
 
-      <a className="mr-2" href="/about">About</a>
-      <a className="mr-2" href="/pricing">Pricing</a>
-      <a className="mr-2" href="/contact">Contact</a>
-      
+      {
+        navItems.map( navItem => 
+          <Link id={navItem.path} className="mr-2" href={navItem.path}>
+            {navItem.text}
+          </Link>
+        )
+      }
+
     </nav>
   )
 }
+      /**
+      <Link className="mr-2" href="/about">About</Link>
+      <Link className="mr-2" href="/pricing">Pricing</Link>
+      <Link className="mr-2" href="/contact">Contact</Link>
+      */
