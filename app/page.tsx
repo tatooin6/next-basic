b03a24d7baf3ea7be307e8936a42e9b76af1a948
Antonio Pantoja <tato_fos@outlook.com>
2024-04-01 19:51:07 -0400

    22. Add Link to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center p-24">
       <span className="text-5xl">Hola Mundo!!!</span>
+
+      <Link href={'/about'} >About Link</Link>
     </main>
   );
 }
